9f1934f7c47ab90ae2d2760d3a198c254aa6e70c
meidix <mahdihossieni@gmail.com>
2022-05-24 13:07:33 +0430

    just made it a little easier and the size of the qr code can now be configured as well

--- generateQrCode.py
+++ generateQrCode.py
@@ -6,8 +6,8 @@ def main():
     """
     generates a qr code based on the arguments it gets from the command line
     """
-    img = qrcode.make(sys.argv[1], box_size=1)
-    img.save(f"{sys.argv[2]}.png")
+    img = qrcode.make(sys.argv[1], box_size=int(sys.argv[2]))
+    img.save(f"{sys.argv[3]}.png")
 
 
 if __name__ == "__main__":
